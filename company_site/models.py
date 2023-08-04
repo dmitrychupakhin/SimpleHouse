93fa03bb8c7a8484a140ee9722fadfdc37a19bfe
danil <danilvolodimirovich@gmail.com>
2023-08-04 13:36:54 +0200

    Design is ready, Model of Services is created

--- company_site/models.py
+++ company_site/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="services/")
+
+    def __str__(self):
+        return self.title
